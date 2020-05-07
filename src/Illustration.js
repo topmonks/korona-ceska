72e8ca07959c99f4f43b7ad990a4941ec2e534b0
Dan Hromada <hromada.dan@gmail.com>
2020-05-07 09:43:16 +0200

    Fix illustrations path name

--- src/Illustration.js
+++ src/Illustration.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+const getIll = (img) => {
+  try {
+    return require(`./illustrations/${img}.png`)
+  } catch (error) {
+    console.warn('Unknown Illustration', { img });
+  }
+}
+
 export default function Illustration({ img }) {
   return (
     <img
       style={{ height: "320px" }}
       alt="person"
-      src={require(`./illustrations/${img}.png`)}
+      src={getIll(img)}
     />
   );
 }
