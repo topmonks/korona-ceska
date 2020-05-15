e0b6525747b0f505571ea4efd478a6527d14bb4a
Dan Hromada <hromada.dan@gmail.com>
2020-05-15 09:39:18 +0200

    Extract Main Menu

---
+++ src/MainMenu.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-navi";
+
+
+
+export default function MainMenu() {
+  return (
+    <div className="main-menu">
+      <Link className="main-menu__link" href="/hra">
+        <b>Nová hra</b>
+      </Link>
+      <Link className="main-menu__link" href="/story">
+        Příběh
+      </Link>
+      <Link className="main-menu__link" href="/help">
+        pověda
+      </Link>
+      <Link className="main-menu__link" href="/credits">
+        O autorech
+      </Link>
+    </div>
+  );
+}
--- src/ScreenMenu.js
+++ src/ScreenMenu.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { Link } from "react-navi";
+import MainMenu from "./MainMenu";
 
 export default function ScreenMenu({ route }) {
   return (
     <div className="screen screen--menu">
       <h1>Korona Česká</h1>
-      <div className="main-menu">
-        <Link className="main-menu__link" href="/hra">
-          <b>Nová hra</b>
-        </Link>
-        <Link className="main-menu__link" href="/story">
-          Příběh
-        </Link>
-        <Link className="main-menu__link" href="/help">
-          Nápověda
-        </Link>
-        <Link className="main-menu__link" href="/credits">
-          O autorech
-        </Link>
-      </div>
+      <MainMenu />
     </div>
   );
 }
+
