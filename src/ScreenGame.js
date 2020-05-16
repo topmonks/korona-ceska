8acb0727fbe31d727d31b540134a10fdd4f1eb05
Dan Hromada <hromada.dan@gmail.com>
2020-05-16 09:37:53 +0200

    Hide menu button for now

--- src/ScreenGame.js
+++ src/ScreenGame.js
@@ -1,11 +1,11 @@
 import React from "react";
 import GameClient from "./GameClient";
-import { Link } from "react-navi";
+// import { Link } from "react-navi";
 
 export default function ScreenGame() {
   return (
     <div className="screen screen--game">
-      <Link href="/"> Menu</Link>
+      {/* <Link href="/"> Menu</Link> */}
       <GameClient />
     </div>
   );
