9de83ac8611bebe38e3e25cb7164009880f0cd47
jaklradek <jaklradek@gmail.com>
2020-05-18 19:54:43 +0200

    ui elements vh sizes

--- src/GameOver.js
+++ src/GameOver.js
@@ -10,6 +10,7 @@ export default function GameOver({ win, loose, draw }) {
     <div className="game-over">
       {win && (
         <div>
+          <img src={victory} alt="loss" />
           <h1>Epidemie je pod kontrolou</h1>
           <p>
             Občas se objeví pár nových případů, ale informovaný lid, funkční
@@ -18,11 +19,11 @@ export default function GameOver({ win, loose, draw }) {
             země. Svatý Václav je jmenován do čela Světové zdravotnické
             organizace.
           </p>
-          <img src={victory} alt="loss" />
         </div>
       )}
       {loose === 1 && (
         <div>
+          <img src={lossEpidemie} alt="loss" />
           <h1>Epidemie se vymkla z rukou</h1>
           <p>
             Počty nakažených a zesnulých rostou exponenciálním tempem. Vojtík
@@ -32,11 +33,11 @@ export default function GameOver({ win, loose, draw }) {
             vědci vyvinou vakcínu či levný a rychlý způsob léčby; možná nastává
             éra jak z filmu Dvanáct opic. Jisté je pouze to, že jsi selhal.{" "}
           </p>
-          <img src={lossEpidemie} alt="loss" />
         </div>
       )}
       {loose === 2 && (
         <div>
+          <img src={lossHealth} alt="loss" />
           <h1>Zdravotní stav obyvatelstva je kritický</h1>
           <p>
             Boj s epidemií je sice udatný, ale Covid-19 není jediný zdravotní
@@ -46,11 +47,11 @@ export default function GameOver({ win, loose, draw }) {
             umořený strachem a nepracujícím zdravotnictvím rezignuje na vyhlídky
             lepšího zítřka, nastávají temné časy.
           </p>
-          <img src={lossHealth} alt="loss" />
         </div>
       )}
       {loose === 3 && (
         <div>
+          <img src={lossEconomy} alt="loss" />
           <h1>Ekonomika zkolabovala</h1>
           <p>
             Urputný boj s epidemií bez ohledu na náklady přivodil ekonomický
@@ -60,11 +61,11 @@ export default function GameOver({ win, loose, draw }) {
             jsi uvrhl společnost do dlouhého marasmu. Fronta na ilegální přechod
             hranic se vine až po Prachatice.
           </p>
-          <img src={lossEconomy} alt="loss" />
         </div>
       )}
       {loose === 4 && (
         <div>
+          <img src={lossTrust} alt="loss" />
           <h1>Konec demokracie</h1>
           <p>
             Svými chaotickými rozhodnutími jsi definitivně podryl důvěru lidí ve
@@ -74,7 +75,6 @@ export default function GameOver({ win, loose, draw }) {
             Skupinka osvícených vytváří Autonomní zónu Holešovice. Václavu zpod
             Blaníka ale není umožněno vstoupit...
           </p>
-          <img src={lossTrust} alt="loss" />
         </div>
       )}
       {draw && (
