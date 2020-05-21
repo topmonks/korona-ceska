91ad64e02a72f46f1e1d9890332488c277da9def
Aleš Roubíček <ales@roubicek.name>
2020-05-21 06:38:55 +0200

    Layout WIP

--- src/ScreenMenu.js
+++ src/ScreenMenu.js
@@ -4,17 +4,19 @@ import ScreenButton from "./ScreenButton";
 export default function ScreenMenu({ route }) {
   return (
     <div className="screen screen--menu">
-      <h1>Korona Česká</h1>
-      <div>
-        <ScreenButton primary path="/hra">
-          <b>Nová hra</b>
-        </ScreenButton>
-        <ScreenButton path="/help">
-          Nápověda
-        </ScreenButton>
-        <ScreenButton path="/credits">
-          O autorech
-        </ScreenButton>
+      <div className="bgio-client">
+        <h1>Korona Česká</h1>
+        <div className="menu">
+          <ScreenButton primary path="/hra">
+            <b>Nová hra</b>
+          </ScreenButton>
+          <ScreenButton path="/help">
+            Nápověda
+          </ScreenButton>
+          <ScreenButton path="/credits">
+            O autorech
+          </ScreenButton>
+        </div>
       </div>
     </div>
   );
