55349fdc79bfe4a21bb9249264c8d0530b6cce0d
Dan Hromada <hromada.dan@gmail.com>
2020-05-21 08:32:06 +0200

    Revert "Change buttons style"
    This reverts commit 38b608deae30389c8e11d489e1dc40331adb1b6e.

--- src/ScreenMenu.js
+++ src/ScreenMenu.js
@@ -4,19 +4,17 @@ import ScreenButton from "./ScreenButton";
 export default function ScreenMenu({ route }) {
   return (
     <div className="screen screen--menu">
-      <div className="bgio-client">
-        <h1>Korona Česká</h1>
-        <div className="menu">
-          <ScreenButton primary path="/hra">
-            <b>Nová hra</b>
-          </ScreenButton>
-          <ScreenButton path="/help">
-            Nápověda
-          </ScreenButton>
-          <ScreenButton path="/credits">
-            O autorech
-          </ScreenButton>
-        </div>
+      <h1>Korona Česká</h1>
+      <div>
+        <ScreenButton primary path="/hra">
+          <b>Nová hra</b>
+        </ScreenButton>
+        <ScreenButton primary path="/help">
+          Nápověda
+        </ScreenButton>
+        <ScreenButton primary path="/credits">
+          O autorech
+        </ScreenButton>
       </div>
     </div>
   );
