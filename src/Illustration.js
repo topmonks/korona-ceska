8fce46935ad3ed24f9f0ae8b4d5b82d0b1c782b5
Aleš Roubíček <ales@roubicek.name>
2020-05-22 12:52:29 +0200

    Use actual image size and DPR only to prevent request mainheim on iOS

--- src/Illustration.js
+++ src/Illustration.js
@@ -2,13 +2,13 @@ import React from "react";
 import images from "./cloudinary.json";
 
 const screenSizes = [
-  ["(max-height: 455px)", 32],
-  ["(min-height: 456px) and (max-height: 555px)", 136],
-  ["(min-height: 556px) and (max-height: 667px)", 162],
-  ["(min-height: 668px) and (max-height: 735px)", 202],
-  ["(min-height: 736px) and (max-height: 811px)", 242],
-  ["(min-height: 812px) and (max-height: 1023px)", 260],
-  ["(min-height: 1024px)", 400]
+  ["only screen and (max-height: 455px)", 32],
+  ["only screen and (min-height: 456px) and (max-height: 555px)", 126],
+  ["only screen and (min-height: 556px) and (max-height: 667px)", 162],
+  ["only screen and (min-height: 668px) and (max-height: 735px)", 202],
+  ["only screen and (min-height: 736px) and (max-height: 811px)", 242],
+  ["only screen and (min-height: 812px) and (max-height: 1023px)", 260],
+  ["only screen and (min-height: 1024px)", 400]
 ];
 
 function addTransformations(src, dpr, height) {
@@ -16,7 +16,7 @@ function addTransformations(src, dpr, height) {
 }
 
 export function* getIllustrationsUrls(dpr) {
-  for (let {secure_url: src} of Object.values(images)) {
+  for (let { secure_url: src } of Object.values(images)) {
     for (let [media, height] of screenSizes) {
       if (window.matchMedia(media).matches) {
         yield addTransformations(src, dpr, height);
@@ -25,28 +25,21 @@ export function* getIllustrationsUrls(dpr) {
   }
 }
 
-const imgUrl = (img, { height, dpr }) => {
+const imgUrl = (img, { dpr }) => {
   const src = images[`illustrations/${img}.png`]["secure_url"];
-  return addTransformations(src, dpr, height);
+  for (let [media, height] of screenSizes) {
+    if (window.matchMedia(media).matches) {
+      return addTransformations(src, dpr, height);
+    }
+  }
 };
 
-const imgSrcSet = (img, { height, dpr = window.devicePixelRatio }) =>
-  `${imgUrl(img, { height, dpr: 1 })} 1x,
-   ${imgUrl(img, { height, dpr: 1.5 })} 1.5x,
-   ${imgUrl(img, { height, dpr })} ${dpr}x`;
-
 export default function Illustration({ img, alt }) {
+  const dpr = window.devicePixelRatio;
   return (
-    <picture>
-      {screenSizes.map(([media, height]) => (
-        <source key={height}
-          media={media}
-          srcSet={imgSrcSet(img, { height })}/>
-      ))}
-      <img className="illustration" alt={alt || img}
-           src={imgUrl(img, { height: 640, dpr: "auto" })}
-           width="480" height="640"/>
-    </picture>
+    <img className="illustration" alt={alt || img}
+         srcSet={`${imgUrl(img, { dpr })} ${dpr}x`}
+         width="480" height="640" />
   );
 }
 
