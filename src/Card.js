2e85081758680e740e9c25864689c2ad0bf60601
jaklradek <jaklradek@gmail.com>
2020-05-23 14:58:56 +0200

    basic animation

--- src/Card.js
+++ src/Card.js
@@ -6,24 +6,26 @@ import {
   makeClass as css,
 } from "./library";
 import Illustration from "./Illustration";
-import { useTransition, animated, config } from 'react-spring';
-
-
+import { useTransition, animated, config } from "react-spring";
 
 export default function Card({ card, answer, effect, week }) {
   const isIncident = useMemo(() => isEventCard(card), [card]);
   const isStory = useMemo(() => isStoryCard(card), [card]);
 
+  const transitions = useTransition(
+    `${week}-${effect}-${card?.img}`,
+    (p) => p,
+    {
+      from: { opacity: 0, transform: "scale(0.95)" },
+      enter: { opacity: 1, transform: "scale(1)" },
+      leave: { opacity: 0, transform: "scale(1)", display: "none" },
+      config: { ...config.wobbly, friction: 22 },
+    }
+  );
 
-  const transitions = useTransition(`${week}-${effect}-${card?.img}`, p => p, {
-    from: { opacity: 1, transform: 'scale(0.8)' },
-    enter: { opacity: 1, transform: 'scale(1)' },
-    leave: { opacity: 0, transform: 'scale(0.9)', display: 'none' },
-    config: { ...config.wobbly, friction: 22 }
-  });
-
-  const cardWeekNumber = !isStory ? <div className="card__week">{week}. týden</div> : null;
-
+  const cardWeekNumber = !isStory ? (
+    <div className="card__week">{week}. týden</div>
+  ) : null;
 
   if (effect) {
     return (
@@ -44,7 +46,6 @@ export default function Card({ card, answer, effect, week }) {
     return null;
   }
 
-
   return (
     <div
       className={css(
@@ -57,15 +58,11 @@ export default function Card({ card, answer, effect, week }) {
       {transitions.map(({ props, key }) => (
         <animated.div key={key} style={props} className="card__content">
           {!isIncident && card.img && (
-            <div
-              className={css("card__img", isStory && "card__img--story")}
-            >
+            <div className={css("card__img", isStory && "card__img--story")}>
               <Illustration img={card.img} />
             </div>
           )}
-          <h3 className="card__name">
-            {card.name}
-          </h3>
+          <h3 className="card__name">{card.name}</h3>
           <p
             className={css("card__text", isStory && "card__text--story")}
             dangerouslySetInnerHTML={{ __html: card.text }}
@@ -74,6 +71,4 @@ export default function Card({ card, answer, effect, week }) {
       ))}
     </div>
   );
-
-
 }
