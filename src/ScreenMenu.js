e6ef76befea08a232840666bda67c1cea0847ca3
jaklradek <jaklradek@gmail.com>
2020-05-25 11:38:14 +0200

    top menu logo

--- src/ScreenMenu.js
+++ src/ScreenMenu.js
@@ -1,36 +1,33 @@
-import React, { useEffect, useMemo, } from "react";
+import React, { useEffect, useMemo } from "react";
 import ScreenButton from "./ScreenButton";
 import { resetBodyGamePlay, makeShareHandler, scrollToTop } from "./library";
-import { setShowKoronaStoryNewbie, showKoronaStoryNewbie, } from "./GameKorona";
-
+import { setShowKoronaStoryNewbie, showKoronaStoryNewbie } from "./GameKorona";
+import logoKorona from "./icons/logo.svg";
 
 export default function ScreenMenu({ route }) {
   useEffect(resetBodyGamePlay(), []);
-  useEffect(scrollToTop, [])
-  const share = useMemo(makeShareHandler, [])
-
+  useEffect(scrollToTop, []);
+  const share = useMemo(makeShareHandler, []);
 
   return (
     <div className="screen screen--menu">
-      <h1>Korona Česká</h1>
+      <img className="logo--menu" src={logoKorona} alt="korona logo" />
       <div>
         <ScreenButton primary path="/hra">
           <b>Nová hra</b>
         </ScreenButton>
         {!showKoronaStoryNewbie() && (
           <ScreenButton
-            onClick={() => { setShowKoronaStoryNewbie(true) }}
+            onClick={() => {
+              setShowKoronaStoryNewbie(true);
+            }}
             path="/hra"
           >
             Začít s příběhem
           </ScreenButton>
         )}
-        <ScreenButton path="/help">
-          Nápověda
-        </ScreenButton>
-        <ScreenButton path="/credits">
-          O autorech
-        </ScreenButton>
+        <ScreenButton path="/help">Nápověda</ScreenButton>
+        <ScreenButton path="/credits">O autorech</ScreenButton>
         {share && (
           <ScreenButton onClick={() => share()}>
             <b>Sdílet</b>
@@ -40,4 +37,3 @@ export default function ScreenMenu({ route }) {
     </div>
   );
 }
-
