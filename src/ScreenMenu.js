054607b74def05045de9abb7e5eb5707cd8fee45
Aleš Roubíček <ales@roubicek.name>
2020-05-25 14:54:19 +0200

    Add TopMonks logo to menu footer

--- src/ScreenMenu.js
+++ src/ScreenMenu.js
@@ -3,6 +3,7 @@ import ScreenButton from "./ScreenButton";
 import { resetBodyGamePlay, makeShareHandler, scrollToTop } from "./library";
 import { setShowKoronaStoryNewbie, showKoronaStoryNewbie } from "./GameKorona";
 import logoKorona from "./icons/logo.svg";
+import logoTm from "./icons/tm-logo.svg";
 
 export default function ScreenMenu({ route }) {
   useEffect(resetBodyGamePlay(), []);
@@ -11,7 +12,7 @@ export default function ScreenMenu({ route }) {
 
   return (
     <div className="screen screen--menu">
-      <img className="logo--menu" src={logoKorona} alt="korona logo" />
+      <img className="logo--menu" src={logoKorona} alt="Korona Česká" />
       <div>
         <ScreenButton primary path="/hra">
           <b>Nová hra</b>
@@ -34,6 +35,10 @@ export default function ScreenMenu({ route }) {
           </ScreenButton>
         )}
       </div>
+      <footer style={{color: "#fff", lineHeight: "48px"}}>
+        © 2020
+        <a href="https://www.topmonks.cz/"><img className="tm-logo" src={logoTm} alt="TOPMONKS" /></a>
+      </footer>
     </div>
   );
 }
