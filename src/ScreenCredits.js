337fdf2d11e5161c0d978edd7e0dbcb2f939fac1
jaklradek <jaklradek@gmail.com>
2020-05-25 17:05:16 +0200

    new credits

--- src/ScreenCredits.js
+++ src/ScreenCredits.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import ScreenButton from './ScreenButton';
+import ScreenButton from "./ScreenButton";
 import logo from "./icons/tm-logo.svg";
 import { resetBodyGamePlay } from "./library";
 
@@ -28,22 +28,48 @@ export default function ScreenCredits({ route }) {
           </a>
         </div>
       </div>
-      <div className="screen--credits__row">
-        <h3>Radek Jakl</h3>
-        <p>Ilustrace, grafika a herní design</p>
+      <div className="screen--credits__main">
+        <div className="screen--credits__row">
+          <h3>Radek Jakl</h3>
+          <p>Ilustrace, grafika a herní design</p>
+        </div>
+        <div className="screen--credits__row">
+          <h3>Josef Tětek</h3>
+          <p>Herní design a texty</p>
+        </div>
+        <div className="screen--credits__row">
+          <h3>Dan Hromada</h3>
+          <p>Programování</p>
+        </div>
+        <div className="screen--credits__row">
+          <h3>Aleš Roubíček</h3>
+          <p>Programování</p>
+        </div>
+        <div className="screen--credits__row">
+          <h3>Lea Petrášová</h3>
+          <p>Projekt, texty</p>
+        </div>
+        <div className="screen--credits__row">
+          <h3>Eva Pénzeš</h3>
+          <p>Marketing</p>
+        </div>
       </div>
       <div className="screen--credits__row">
-        <h3>Josef Tětek</h3>
-        <p>Herní design a texty</p>
+        <h3>Testeři</h3>
+        <p>
+          Veronika Hallerová, Tomáš Brejla, Kateřina Mareková a mnoho dalších.
+        </p>
       </div>
       <div className="screen--credits__row">
-        <h3>Dan Hromada</h3>
-        <p>Programování</p>
+        <h3>Speciální poděkování</h3>
+        <br />
+        <p>Nicolo John Davis, autor herniho frameworku boardgame.io</p>
+        <br />
+        <p>Tomáš Franěk, iSymbio Accessibility Přístupnost pro nevidomé</p>
+        <br />
+        <p>Velkou inspirací nám byla hra Reigns od Nerial</p>
       </div>
-      <div className="screen--credits__row">
-        <h3>Lea Petrášová</h3>
-        <p>Projekt, texty</p>
-      </div>
+
       <ScreenButton>Zpět na menu</ScreenButton>
     </div>
   );
